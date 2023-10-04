629872cce514982d739417300f577d98e1b897e6
HyunJun Baek <hjbaek5796@gmail.com>
2023-10-04 20:23:37 +0900

    [Implicit Animations] explicit_animations_screen start

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animations_practice/screens/explicit_animations_screen.dart';
 import 'package:flutter_animations_practice/screens/implicit_animations_screen.dart';
 
 class MenuScreen extends StatelessWidget {
@@ -26,10 +27,19 @@ class MenuScreen extends StatelessWidget {
               onPressed: () {
                 _goToPage(
                   context,
-                  ImplicitAnimationsScreen(),
+                  const ImplicitAnimationsScreen(),
                 );
               },
               child: const Text("Implicit Animations"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _goToPage(
+                  context,
+                  const ExplicitAnimationsScreen(),
+                );
+              },
+              child: const Text("Explicit Animations"),
             ),
           ],
         ),
